19417eb0844f763d79b5ebf48bc4fcebd0f1bd47
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-12 22:09:03 -0400

    Send text to Input text box

--- SeleniumExampleProject/SeleniumExampleProject/Program.cs
+++ SeleniumExampleProject/SeleniumExampleProject/Program.cs
@@ -5,20 +5,22 @@ using System.Threading;
 
 class Program
 {
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement element;
+    static IWebElement textBox;
+    
     static void Main()
     {
-        IWebDriver driver = new ChromeDriver();
-
         driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
 
         driver.Manage().Window.Maximize();
 
         Thread.Sleep(3000);
 
+        //Element can be found by name, id, CSS selector, XPath
         try
         {
-            //Element can be found by name, id, CSS selector, XPath
-            IWebElement element = driver.FindElement(By.Name("Initial"));
+            element = driver.FindElement(By.Name("Initial"));
 
             if (element.Displayed)
             {
@@ -30,8 +32,24 @@ class Program
         {
             //System.Console.WriteLine("No I cannot see the element!!");
             RedMessage("No I cannot see the element!!");            
-        }       
+        }
 
+        textBox = driver.FindElement(By.Id("Initial"));
+        textBox.SendKeys("S");
+        Console.WriteLine("Initial is: " + textBox.GetAttribute("value"));
+
+        textBox = driver.FindElement(By.Id("FirstName"));
+        textBox.SendKeys("Surekha");
+        Console.WriteLine("First Name is: " + textBox.GetAttribute("value"));
+        
+        textBox = driver.FindElement(By.Id("MiddleName"));
+        textBox.SendKeys("None");
+        Console.WriteLine("Middle Name is: " + textBox.GetAttribute("value"));
+
+        Thread.Sleep(5000);
+
+        //textBox.Clear(); //clears the text in the text box 
+        
         driver.Quit();
     }
 
