910be698e708ff1e986078cea4e2186125e51464
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-12 21:14:40 -0400

    Find element by name and message displayed in color

--- SeleniumExampleProject/SeleniumExampleProject/Program.cs
+++ SeleniumExampleProject/SeleniumExampleProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 
@@ -10,8 +11,38 @@ class Program
 
         driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
 
+        driver.Manage().Window.Maximize();
+
         Thread.Sleep(3000);
 
+        //Element can be found by name, id, CSS selector, XPath
+        IWebElement element = driver.FindElement(By.Name("Initial"));
+
+        if (element.Displayed)
+        {
+            //System.Console.WriteLine("Yes I can see the element!!");
+            GreenMessage("Yes I can see the element!!");
+        }
+        else
+        {
+            //System.Console.WriteLine("No I cannot see the element!!");
+            RedMessage("No I cannot see the element!!");
+        }
+
         driver.Quit();
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }
