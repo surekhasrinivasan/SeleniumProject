d4fe1b2ac41b616de335a8eadd74709cb9bf7fb7
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-12 20:49:58 -0400

    Set up environment - initial commit

---
+++ SeleniumExampleProject/SeleniumExampleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
