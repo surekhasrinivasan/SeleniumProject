37bc8d60f72e106efaca4c0b64db13a0526187e4
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-12 22:53:40 -0400

    Radio button element

--- SeleniumExampleProject/SeleniumExampleProject/Program.cs
+++ SeleniumExampleProject/SeleniumExampleProject/Program.cs
@@ -8,6 +8,7 @@ class Program
     static IWebDriver driver = new ChromeDriver();
     static IWebElement element;
     static IWebElement textBox;
+    static IWebElement radioButton;
     
     static void Main()
     {
@@ -34,12 +35,13 @@ class Program
             RedMessage("No I cannot see the element!!");            
         }
 
+        //Input text in the text box
         textBox = driver.FindElement(By.Id("Initial"));
         textBox.SendKeys("S");
         Console.WriteLine("Initial is: " + textBox.GetAttribute("value"));
 
         textBox = driver.FindElement(By.Id("FirstName"));
-        textBox.SendKeys("Surekha");
+        textBox.SendKeys("Smith");
         Console.WriteLine("First Name is: " + textBox.GetAttribute("value"));
         
         textBox = driver.FindElement(By.Id("MiddleName"));
@@ -48,7 +50,22 @@ class Program
 
         Thread.Sleep(5000);
 
-        //textBox.Clear(); //clears the text in the text box 
+        //textBox.Clear(); //clears the text in the text box
+        //textBox.GetAttribute("maxlength"); //gives the maximum length of characters the text box can take
+
+        //Radio Button
+        radioButton = driver.FindElement(By.CssSelector("#details > table > tbody > tr:nth-child(5) > td:nth-child(2) > input[type=\"radio\"]:nth-child(1)"));
+
+        if (radioButton.GetAttribute("checked") == "true")
+        {
+            Console.WriteLine("The radio button is checked and its value is: " + radioButton.GetAttribute("value"));
+        }
+        else
+        {
+            Console.WriteLine("The radio button is NOT checked");
+        }
+
+        Thread.Sleep(5000);
         
         driver.Quit();
     }
